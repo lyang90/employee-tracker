b2338b43108f617fc8d5b6002a34a5a044df2542
lyang7305 <lyang7305@gmail.com~>
2024-07-22 10:00:23 -0500

    added database and seeds to files

---
+++ schema.sql
@@ -0,0 +1,28 @@
+DROP IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+\c employees_db;
+
+CREATE TABLE department(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
+)
+
+CREATE TABLE role(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER 
+    FOREIGN KEY (id)
+    REFERENCES (department)
+)
+
+CREATE TABLE employee(
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER 
+    FOREIGN KEY (id)
+    REFERENCES(role),
+    manager_id INTEGER
+)
