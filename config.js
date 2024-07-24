fcf8a61f65ba721709c9ac74bb22c96be8034ad6
lyang7305 <lyang7305@gmail.com~>
2024-07-23 22:30:05 -0500

    fixed typos in env and config file

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const pool = new Pool(
     {
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
-        host: process.env.HOST,
+        host: process.env.DB_HOST,
         database: process.env.DB_DATABASE
     },
     console.log('Connected to employees database.')
